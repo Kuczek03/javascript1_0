f1469b86de9bb2d25d6131bf948f52064c503a1a
Wiktor Kuczek <117631329+Kuczek03@users.noreply.github.com>
2024-10-02 10:00:31 +0000

    Cm1_1_2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-(function () {
-  //TODO
+(function () { 
+document.getElementById("ex1_button").addEventListener("click", function() {
+   
+    let numbers = Array.from({length: 10}, (_, i) => i).join(", ");
+
+    document.getElementById("ex1_content").textContent = numbers;
+});
+
+let phone_numb = []
+phone_numb = document.getElementById("ex2_text").value ;
+document.getElementById("ex2_content").textContent = phone_numb;
 })();
